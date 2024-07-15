7717cd5c0a94bc8bf000ce824a00e6c232e61252
Abdul Vaheed <124774319+vaheed9440@users.noreply.github.com>
2024-07-15 10:37:44 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -11,8 +11,8 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket cloudanddevopsbyraham0073456lfkcv.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456lfkcv.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456lfkcv.k8s.local
-kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
+aws s3api create-bucket --bucket cloudanddevopsbyraham00734.k8s.local --regionap-south-1
+aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham00734.k8s.local --regionap-south-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://cloudanddevopsbyraham00734.k8s.local
+kops create cluster --name rahams.k8s.local --zonesap-south-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
 kops update cluster --name rahams.k8s.local --yes --admin
